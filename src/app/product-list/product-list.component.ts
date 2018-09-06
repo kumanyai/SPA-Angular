0a73cf13a2f87be9b420d9f0b65a1c1df37ba865
MiniSony <warrentalex26ies@gmail.com>
2018-09-06 16:56:41 -0600

    Respuestas HTTP

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,54 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import {IProduct} from "./product";
-import {ProductService} from "./product.service";
+import {IProduct} from './product';
+import {ProductService} from './product.service';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
 
-  pagetittle: string = 'Lista de producto';
-  imageWidth:number = 50;
-  imageMargin: number = 2;
-  showImage: boolean = false;
-  _listFilter:string;
+  pagetittle = 'Lista de producto';
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage = false;
+  errorMessage:string;
 
-  get listFilter(): string{
+  _listFilter: string;
+  get listFilter(): string {
     return this._listFilter;
   }
 //
-  set listFilter(value:string){
+  set listFilter(value: string) {
     this._listFilter = value;
     this.filterProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
   }
 
-  filterProducts:IProduct[];
-  products:IProduct[] = [];
+  filterProducts: IProduct[];
+  products: IProduct[] = [];
 
-  constructor(private productService: ProductService){
+  constructor(private productService: ProductService) {
 
     // this.listFilter = 'cart'
   }
 
-  onRatingClicked(message:string):void{
+  onRatingClicked(message: string): void {
     this.pagetittle = `Lista de Productos ${message}`;
   }
 
-  performFilter(filterBy:string):IProduct[]{
+  performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLowerCase(); //Convertirmos los criterios de filtros a minusculas Se hace para no tener problemas con mayusculas
-    return this.products.filter((product:IProduct) => //Decolvemos la lista filtrada
-      product.productName.toLocaleLowerCase().indexOf(filterBy) != -1);
+    return this.products.filter((product: IProduct) => //Decolvemos la lista filtrada
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
   }
 
-  toggleImage():void{
+  toggleImage(): void {
     this.showImage = !this.showImage;
   }
+  //ESTA CLASE SIMPLEMENTE SE SUSCRIBE AL OBSERVABLE QUE ESTA EN EL PRODUCT.SERVICE Y ESPERA DATOS O UNA NOTIFICACION
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(
+      products => {
+        this.products = products;
+        this.filterProducts = this.products;
+      },
+      error => this.errorMessage = <any>error //la <any> es un operador de casting es decir estamos emitiendo el error de devuelto
+    );
 
-  ngOnInit():void{
-    this.products = this.productService.getProducts();
-    this.filterProducts = this.products;
   }
 
 }
--- src/app/product-list/product.service.ts
+++ src/app/product-list/product.service.ts
@@ -1,52 +1,40 @@
 ///<reference path="../../../node_modules/@angular/core/src/di/injectable.d.ts"/>
 import { Injectable } from '@angular/core';
-import {IProduct} from "./product";
+import { IProduct } from './product';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import { catchError, tap  } from 'rxjs/operators'
+import {el} from '@angular/platform-browser/testing/src/browser_util';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  getProducts(): IProduct[]{
-    return[
-      {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-      },
-      {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-      },
-      {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2016",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-      },
-      {
-        "productId": 8,
-        "productName": "Saw",
-        "productCode": "TBX-0022",
-        "releaseDate": "May 15, 2016",
-        "description": "15-inch steel blade hand saw",
-        "price": 11.55,
-        "starRating": 3.7,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-      }];
+
+  private productUrl = 'api/products/products.json';
+
+  constructor(private http: HttpClient) {}
+  //LA FORMA CORRECTA DE USAR EL HTTP ES ENCAPSULARLO EN UN SERVISIO Y LUEGO PONER UN OBSERVABLE
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log(`ALL: ${JSON.stringify(data)}`)),
+      catchError(this.handleError)
+    );
   }
+
+  private handleError(err: HttpErrorResponse){
+    //En una app real, nosotros necesitamos enviar algun dato o login a nuestro servidor
+    let errorMessage = ``;
+    if (err.error instanceof ErrorEvent){
+      //Si en el lado del cliente ocurre un error, Handle dara el aviso
+      errorMessage = `A ocurrido un error: ${err.error.message}`;
+    } else {
+      //El servidor(backend) retorna un codigo de error que no se dio la conexion
+      //La respuesta la contiene el body cuando algo sale mal
+      errorMessage = `Codigo retornado por el servidor: ${err.status}, El mensaje de error es: ${err.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+  
 }
