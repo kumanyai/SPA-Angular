b43aee5117cba66398e232a27740d6e0effb5170
MiniSony <warrentalex26ies@gmail.com>
2018-09-11 09:58:43 -0600

    Navegacion y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,30 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
 import { StartComponent } from './shared/start.component';
 import { ProductService } from './product-list/product.service';
+import { ProductDetailComponent } from './product-list/product-detail.component';
+import {WelcomeComponent} from './home/welcome.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacePipe,
-    StartComponent
+    StartComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: "products", component: ProductListComponent},
+      {path: "products/:id", component: ProductDetailComponent},
+      {path: "welcome", component: WelcomeComponent},
+      {path: "", redirectTo: 'welcome', pathMatch: 'full'},
+      {path: "**", redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
